90b1e1b61f01073ec976109fc595dac412038a69
krystianburka <krystian.burka@gmail.com>
2023-03-07 21:10:44 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const backround = document.querySelector(".color");
+const changeColor = document.querySelector(".change-color");
+
+// const btn = document.querySelector("button");
+changeColor.addEventListener("click", (event) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  console.log(getRandomHexColor());
+  backround.textContent = getRandomHexColor();
+});
+
+// const textInput = document.querySelector("#name-input");
+// const textOutput = document.querySelector("#name-output");
+
+// textInput.addEventListener("input", (event) => {
+//   if (textInput.value === "") {
+//     textOutput.textContent = "Anonymous";
+//   } else {
+//     textOutput.textContent = event.currentTarget.value;
+//   }
+// });
